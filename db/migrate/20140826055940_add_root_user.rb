8d0a98aff17746b7b1b8e5b0475b449add714ae8
Nitin Chauhan <goldy.nitin@gmail.com>
2014-08-29 13:50:14 +0530

    Update Gemfile.lock for Heroku

--- db/migrate/20140826055940_add_root_user.rb
+++ db/migrate/20140826055940_add_root_user.rb
@@ -1,7 +1,10 @@
 class AddRootUser < ActiveRecord::Migration
   def self.up
     user = User.create( :username => 'admin', :email => 'admin@admin.com', :login => 'root', :password => 'pass@123', :password_confirmation => 'pass@123' )
+    
+    user.skip_confirmation!
     user.confirm!
+    user.save
   end
 
   def self.down
