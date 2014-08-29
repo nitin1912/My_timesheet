767a34599a71d2157b75ab9241ad627090004f44
Nitin Chauhan <goldy.nitin@gmail.com>
2014-08-29 15:14:03 +0530

    root user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,7 @@ Timesheet::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  config.action_mailer.default_url_options = {host: 'demo-timesheet.herokuapp.com'}
-
+ 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -49,7 +48,7 @@ Timesheet::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-
+  
   # Disable delivery errors, bad email addresses will be ignored
    config.action_mailer.raise_delivery_errors = true
    config.action_mailer.perform_deliveries = true
@@ -62,6 +61,8 @@ Timesheet::Application.configure do
     password:             'Pass@123',
     authentication:       'plain',
     enable_starttls_auto: true  }
+     config.action_mailer.default_url_options = {host: 'demo-timesheet.herokuapp.com'}
+
   # Enable threaded mode
   # config.threadsafe!
 
