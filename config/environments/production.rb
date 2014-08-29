a041e15bbdddd03389dd145dd1a3181037afad03
Nitin Chauhan <goldy.nitin@gmail.com>
2014-08-29 14:22:38 +0530

    root user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Timesheet::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'demo-timesheet.herokuapp.com',
     user_name:            'app.send.emails@gmail.com',
     password:             'Pass@123',
     authentication:       'plain',
